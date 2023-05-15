c6f3d63b1516767cdad37354903a83397510f617
Adam Howard <adam.howard@vet-ai.com>
2023-05-15 13:56:04 +0100

    revert back to jvm, use jsoup this time

--- game-files-processor/build.gradle.kts
+++ game-files-processor/build.gradle.kts
@@ -1,53 +1,29 @@
-import java.io.IOException
-
 plugins {
-    kotlin("js") version "1.8.20"
+    kotlin("jvm") version "1.8.20"
+    application
 }
 
 group = "com.gitlab.hurtling"
 version = "0.1"
 
 repositories {
+    google()
     mavenCentral()
 }
 
-kotlin {
-    js {
-        binaries.executable()
-        nodejs {
-
-        }
-    }
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jsoup:jsoup:1.16.1")
 }
-task("convertTextFilesToStrings") {
-    println(projectDir.absolutePath)
-    val resDir = "src/main/resources"
-    val cubeBlocksDir = "$resDir/CubeBlocks"
-    val cubeBlocksListing = File(cubeBlocksDir).listFiles()
 
-    val outputDir = File(rootDir, "src/main/kotlin/generated")
-    if( !outputDir.mkdirs() && !outputDir.isDirectory) {
-        throw IOException("couldn't create output dir for sbc files")
-    }
+tasks.test {
+    useJUnitPlatform()
+}
 
-    fun File.writeOut(): String {
-        if(!this.exists()) {
-            throw IOException("file not found: ${this.absolutePath}")
-        }
-        val className = this.nameWithoutExtension.replace(".", "")
-        val fileName = "$className.kt"
-        val outputFile = File(outputDir, fileName)
-        val content = resources.text.fromFile(this).asReader().readText()
-        outputFile.writeText("package generated\nval $className = \"\"\"$content\"\"\"\n")
-        return className
-    }
+kotlin {
+    jvmToolchain(11)
+}
 
-    val cubeBlocksList = File(outputDir, "CubeBlocksList.kt")
-    File(resDir, "Components.sbc").writeOut()
-    File(resDir, "Localization/MyTexts.resx").writeOut()
-    cubeBlocksList.writeText("package generated\nval cubeBlocksList = listOf(Components, MyTexts, ")
-    for( file in cubeBlocksListing) {
-        cubeBlocksList.appendText(" ${file.writeOut()},")
-    }
-    cubeBlocksList.appendText(")")
+application {
+    mainClass.set("MainKt")
 }
