b82c9d69d81d4039147cd0b99e971962c3633f1e
Adam Howard <adam.howard@vet-ai.com>
2023-05-15 15:35:07 +0100

    plugging away nicely at  processor impl

--- game-files-processor/src/main/kotlin/Main.kt
+++ game-files-processor/src/main/kotlin/Main.kt
@@ -1,33 +1,75 @@
+import generated.MyTexts
 import generated.cubeBlocksList
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
-private val allBlockDefs: MutableList<Element> = mutableListOf()
 private val allBlockData: MutableList<BlockData> = mutableListOf()
-fun init() {
-    val cubeBlocksXmlDocs: List<Element> = cubeBlocksList.map {
-        Jsoup.parse(it).root()
+private val localisationStringsMappings = mutableMapOf<String, String>()
+
+/**Blocks in CubeBlocks_Armor_2 are missing their PCU entry. But in the game it's 1. So just return 1*/
+fun Element.getPcuWithFallbackForArmor2(file: String, subtypeId: String): Int? {
+    /*val manualPcuLookups = mapOf(
+        "LadderShaft" to
+    )*/
+    val pcu = this.getElementsByTag("PCU").firstOrNull()?.ownText()?.toInt()
+        ?: return if(file == "CubeBlocks_Armor_2") 1
+        else if(false) {
+            null
+        } else {
+            null
+        }
+    return pcu
+}
+fun initLocalisationStringMappings() {
+    val doc = Jsoup.parse(MyTexts).root()
+    val entries = doc.getElementsByTag("data")
+    println("i18n entries: ${entries.size}")game-files-processor/build.gradle.kts
+    for(entry in entries) {
+        val key = entry.attr("name")
+        val value = entry.getElementsByTag("value").firstOrNull()?.ownText()
+        if(value == null) {
+            println("WARNING value for $key is missing")
+            continue
+        }
+        localisationStringsMappings[key] = value
     }
-    for(definitionsFile in cubeBlocksXmlDocs) {
+}
+//passage is the only block whose subtypeid is blank
+fun initCubeBlockDefinitions() {
+    val cubeBlocksXmlDocs: Map<String, Element> = cubeBlocksList.mapValues {
+        Jsoup.parse(it.value).root()
+    }
+
+    for((file, definitionsFile) in cubeBlocksXmlDocs) {
         val blockDefs: Element = definitionsFile.select("Definitions>CubeBlocks").firstOrNull()?: throw Exception ("Block defs not found")
-        println("block defs:"+blockDefs.children().size)
+        //println("${blockDefs.children().size} block defs in $file")
         for (block in blockDefs.children()) {
-            val subtypeId = block.getElementsByTag("SubtypeId").firstOrNull()?: throw Exception("couldn't find subtype id")
-            val pcu = block.getElementsByTag("PCU").firstOrNull()?: throw Exception("couldn't find PCU for ${block.tagName()}")
-            val displayName = block.getElementsByTag("DisplayName").firstOrNull()?: throw Exception("couldn't find DisplayName")
+            //I thought I could uniquely identify all blocks by just their subtypeid, but no
+            val subtypeId = block.getElementsByTag("SubtypeId").firstOrNull()?: throw Exception("couldn't find subtype id in $file")
+            val typeId = block.getElementsByTag("TypeId").firstOrNull()?: throw Exception("couldn't find type id in $file")
+            val pcu: Int? = block.getPcuWithFallbackForArmor2(file, subtypeId.ownText())
+            if(pcu == null) {
+                println("WARNING: couldn't find PCU for '${subtypeId.ownText()}' in $file")
+                continue
+            }
+            val displayName = block.getElementsByTag("DisplayName").firstOrNull()
+            if(displayName == null) {
+                println("WARNING: couldn't find DisplayName for '${subtypeId.ownText()}' in $file")
+                continue
+            }
             allBlockData.add(BlockData(
+                type = typeId.ownText(),
                 subtypeId = subtypeId.ownText(),
-                pcu = pcu.ownText().toInt(),
-                displayName = displayName.ownText()
+                pcu = pcu,
+                displayName = displayName.ownText(),
             ))
         }
-        allBlockDefs.addAll(blockDefs.children())
     }
 }
 
 fun main() {
-    init()
+    initLocalisationStringMappings()
     println("all block data:"+allBlockData.size)
     //println(allBlockDefs[451])
 }
