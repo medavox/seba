884f5c175f25d423496343e6636b668743c21ba8
Adam Howard <adam.howard@vet-ai.com>
2023-05-15 13:33:37 +0100

    bring back hacky task

--- game-files-processor/build.gradle.kts
+++ game-files-processor/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.IOException
+
 plugins {
     kotlin("js") version "1.8.20"
 }
@@ -16,4 +18,36 @@ kotlin {
 
         }
     }
+}
+task("convertTextFilesToStrings") {
+    println(projectDir.absolutePath)
+    val resDir = "src/main/resources"
+    val cubeBlocksDir = "$resDir/CubeBlocks"
+    val cubeBlocksListing = File(cubeBlocksDir).listFiles()
+
+    val outputDir = File(rootDir, "src/main/kotlin/generated")
+    if( !outputDir.mkdirs() && !outputDir.isDirectory) {
+        throw IOException("couldn't create output dir for sbc files")
+    }
+
+    fun File.writeOut(): String {
+        if(!this.exists()) {
+            throw IOException("file not found: ${this.absolutePath}")
+        }
+        val className = this.nameWithoutExtension.replace(".", "")
+        val fileName = "$className.kt"
+        val outputFile = File(outputDir, fileName)
+        val content = resources.text.fromFile(this).asReader().readText()
+        outputFile.writeText("package generated\nval $className = \"\"\"$content\"\"\"\n")
+        return className
+    }
+
+    val cubeBlocksList = File(outputDir, "CubeBlocksList.kt")
+    File(resDir, "Components.sbc").writeOut()
+    File(resDir, "Localization/MyTexts.resx").writeOut()
+    cubeBlocksList.writeText("package generated\nval cubeBlocksList = listOf(Components, MyTexts, ")
+    for( file in cubeBlocksListing) {
+        cubeBlocksList.appendText(" ${file.writeOut()},")
+    }
+    cubeBlocksList.appendText(")")
 }
