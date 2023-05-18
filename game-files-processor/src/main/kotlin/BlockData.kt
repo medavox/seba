99f4327963ff16b79a2b702c2441b097b62fa89a
Adam Howard <adam.howard@vet-ai.com>
2023-05-18 23:32:09 +0100

    remove display name, make xsi type optional

--- game-files-processor/src/main/kotlin/BlockData.kt
+++ game-files-processor/src/main/kotlin/BlockData.kt
@@ -4,25 +4,22 @@
  * mass is calculated, and human name is another lookup*/
 data class BlockData(
     val pcu: Int,
-    val displayName: String,
     val type: String,
     val subtypeId: String,
     val humanName: String,
     val components: Map<String, Int>,
     val mass: Double,
-    val xsiType: String?,
+    val xsiType: String = "",
 ) {
     override fun toString(): String {
         return "BlockData(pcu=$pcu,\n" +
-            "\tdisplayName=\"$displayName\",\n" +
             "\ttype=\"$type\",\n" +
             "\tsubtypeId=\"$subtypeId\",\n" +
             "\thumanName=\"$humanName\",\n" +
-            "\txsiType=\"$xsiType\",\n" +
+                (if(xsiType.isNotEmpty()) "\txsiType=\"$xsiType\",\n" else "") +
             "\tmass=$mass,\n" +
-            "\tcomponents=mapOf(\n" +
-            "\t\t" +components.entries.fold("") {acc, (key, value) -> acc+"\"$key\" to $value, "}+
-            "\t)\n" +
+            "\tcomponents=mapOf(" +components.entries.fold("") {acc, (key, value) -> acc+"\"$key\" to $value, "}+
+            ")\n" +
             ")"
     }
 }
