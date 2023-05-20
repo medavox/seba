341b3a1722aa04b3e9bbd2feef1809592d1df06c
Adam Howard <adam.howard@vet-ai.com>
2023-05-20 21:16:41 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-branch=$(git branch --show-curent)
+#!/bin/bash -x
+
+branch=$(git branch --show-current)
 git stash
 if [[ $branch == "main" ]] ; then
   ./gradlew browserProductionWebpack
@@ -6,20 +8,22 @@ else
   git checkout main
 fi
 
-branch=$(git branch --show-curent)
+branch=$(git branch --show-current)
 if [[ $branch == "main" ]] ; then
   ./gradlew browserProductionWebpack
 else
   echo "couldn't checkout main branch. exiting deploy.sh."
+  exit 1
 fi
 
 git checkout site
-branch=$(git branch --show-curent)
+branch=$(git branch --show-current)
 if [[ $branch != "site" ]] ; then
   echo "couldn't checkout site branch. exiting deploy.sh."
+  exit 1
 fi
 
-cp -f build.distributions/* .
+cp -f build/distributions/* .
 git add index.html style.css seba.js seba.js.map
 git commit -m "deploy site using script"
 git push hub
