20982895e77d8cf554d1e49d35541b6357cd55c9
Adam Howard <adam.howard@vet-ai.com>
2023-05-23 23:50:25 +0100

    fix bugs in CountingMap

--- game-files-processor/src/main/kotlin/CountingMap.kt
+++ game-files-processor/src/main/kotlin/CountingMap.kt
@@ -4,10 +4,10 @@
 open class CountingMap<K>(
     protected val backingMap: MutableMap<K, Int> = mutableMapOf<K, Int>()
 ) : MutableMap<K, Int> {
-    override val size: Int = backingMap.size
-    override val entries: MutableSet<MutableMap.MutableEntry<K, Int>> = backingMap.entries
-    override val keys: MutableSet<K> = backingMap.keys
-    override val values: MutableCollection<Int> = backingMap.values
+    override val size: Int get() = backingMap.size
+    override val entries: MutableSet<MutableMap.MutableEntry<K, Int>> get() = backingMap.entries
+    override val keys: MutableSet<K> get() = backingMap.keys
+    override val values: MutableCollection<Int> get() = backingMap.values
 
     override fun containsKey(key: K): Boolean = backingMap.containsKey(key)
     override fun containsValue(value: Int): Boolean = backingMap.containsValue(value)
@@ -18,7 +18,9 @@ open class CountingMap<K>(
 
     override fun put(key: K, value: Int): Int = backingMap.put(key, value) ?: 0
 
-    override fun putAll(from: Map<out K, Int>) = backingMap.putAll(from)
+    override fun putAll(from: Map<out K, Int>) = from.forEach {
+        backingMap.put(it.key, it.value)
+    }
 
     override fun remove(key: K): Int = backingMap.remove(key) ?: 0
     override fun toString(): String = backingMap.toString()
@@ -28,7 +30,7 @@ open class CountingMap<K>(
         newMap.putAll(this)
         for((key, count) in other) {
             if(newMap.containsKey(key)) {
-                val currentCount = newMap.get(key)
+                val currentCount = this.get(key)
                 newMap.put(key, currentCount + count)
             } else {
                 newMap.put(key, count)
